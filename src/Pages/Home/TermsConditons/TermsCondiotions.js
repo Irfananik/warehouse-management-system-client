0bf8f09a2a532a068c9e000c0b9964f1b16c8e4c
Golam Morshed Bhuiyan <69519988+Irfananik@users.noreply.github.com>
2022-05-01 10:56:27 +0600

    terms and conditions update

--- src/Pages/Home/TermsConditons/TermsCondiotions.js
+++ src/Pages/Home/TermsConditons/TermsCondiotions.js
@@ -2,16 +2,19 @@ import React from 'react';
 
 const TermsCondiotions = () => {
     return (
-        <div>
+        <div className="">
             <h2 className="my-4" style={{ color: 'white' }}>Terms and Conditions</h2>
-            <div style={{ textAlign: 'center' }}>
-                <p style={{ color: '#6C757D' }}>
-                    <li>Order below Tk 10 lak's aren't acceptable</li>
-                    <li>Order below Tk 10 lak's aren't acceptable</li>
-                    <li>Order below Tk 10 lak's aren't acceptable</li>
-                    <li>Order below Tk 10 lak's aren't acceptable</li>
-                    <li>Order below Tk 10 lak's aren't acceptable</li>
-                </p>
+            <div className="w-75 d-flex align-items-center justify-content-center" style={{ textAlign: 'left' }}>
+                <div className="ms-5">
+                    <p style={{ color: '#6C757D', lineHeight: '38px'}}>
+                        <li>Agreement to Terms and Conditions. It is agreed that Company’s Warehouse Receipt Terms and Conditions shall govern the dealings between Company and Customer for all warehousing and storage services. It is furthermore agreed that Company’s Warehouse Receipt Terms and Conditions are electronically published at http://www.sekologistics.com, and may be amended by Company from time to time, without notice. In the event of any conflict between these Warehouse Receipt Terms and Conditions as printed herein and the electronically published Warehouse Receipt Terms and Conditions, the electronically published version shall control. It is expressly understood that this Warehouse Receipt does not cover or apply to any rights, obligations, terms or conditions of the freight forwarding, customs brokerage or other services that Company has provided or may provide to the Customer; and that those separate services shall be governed by their respective Terms and Conditions which are provided separately and are posted on the above referenced website.</li>
+                        <li>Definitions. As used in this Warehouse Receipt</li>
+                        <li>Ownership of Goods. Customer warrants that it is the lawful owner and/or has lawful possession of the Goods tendered for storage. Customer warrants that it has sole legal rights to store Goods tendered, to release Goods, and to instruct Company regarding delivery or disposition of the Goods. Customer agrees to notify all parties acquiring any interest in the Goods of the terms and conditions of this Warehouse Receipt and further agrees to indemnify and hold Company harmless from any claim by third parties relating to the ownership, storage, handling or delivery of Goods, or from any other services provided by Company under this Warehouse Receipt. Such indemnification shall include any legal fees or costs incurred from any claim by a third party, regardless of whether or not litigation is actually filed.</li>
+                        <li> Storage.</li>
+                        <li>Termination of Storage. Company reserves the right to terminate storage and to require the removal of the Goods, or any portion thereof, by giving Customer thirty (30) days advance written notice. Customer shall be responsible for payment of all charges attributable to said Goods within the stated period and for removing the Goods from the warehouse upon payment of all charges. If the Goods are not so removed, Company may exercise its rights under applicable law including but not limited to selling the Goods.</li>
+                        <li>Customer’s Warranties & Tender for Storage.</li>
+                    </p>
+                </div>
             </div>
         </div>
     );
