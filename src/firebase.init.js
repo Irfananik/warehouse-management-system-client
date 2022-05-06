9671be33fe0971eae4128950ef00202b1f7d4384
Golam Morshed Bhuiyan <69519988+Irfananik@users.noreply.github.com>
2022-05-06 18:03:48 +0600

    firebase key convert with env variable

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCQHRwPfpSJJing0mhuKWi-rkw5wPimSS8",
-    authDomain: "smartphone-453a1.firebaseapp.com",
-    projectId: "smartphone-453a1",
-    storageBucket: "smartphone-453a1.appspot.com",
-    messagingSenderId: "1073264010777",
-    appId: "1:1073264010777:web:67757392004e63be04ac88"
+    apiKey:process.env.REACT_APP_apiKey,
+    authDomain:process.env.REACT_APP_authDomain, 
+    projectId:process.env.REACT_APP_projectId, 
+    storageBucket:process.env.REACT_APP_storageBucket, 
+    messagingSenderId:process.env.REACT_APP_messagingSenderId, 
+    appId:process.env.REACT_APP_appId, 
 };
 
 // Initialize Firebase
